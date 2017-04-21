e0331e27d45fc68fddc9d9febb916386acd8e6bc
jc321013 <jared.marcolongo1@gmail.com>
2017-04-21 19:03:32 +1000

    fdgf

--- app/src/test/java/com/example/jc321013/unitconversion/ExampleUnitTest.java
+++ app/src/test/java/com/example/jc321013/unitconversion/ExampleUnitTest.java
@@ -14,4 +14,27 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void conversionMainTest(){
+        double original = 1.0;
+        double centimeters = 1.0;
+        double meters = 100.0;
+        double num = centimeters * meters;
+        assertTrue(original == num);
+
+    }
+
+
+
+    @Test
+    public void conversionSettingsTest(){
+        double original = 30.48;
+        double feet = 1.0;
+        double centimeters = 30.48;
+        double num = centimeters * feet;
+        assertTrue(original == num);
+
+    }
+
 }
